e09e6e10297a6707755cba6c9ade4a019bb9a872
RegisDantas <dantas.regis.m@gmail.com>
2025-12-17 18:13:48 -0300

    added admin function to give credits. improved prompt for more friendly responses

--- app/bot/handlers/admin.py
+++ app/bot/handlers/admin.py
@@ -1,10 +1,18 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, ConversationHandler
 
 from app.database.models.user import User, check_admin
 from app.bot.utils.context_utils import load_user
-from app.database.repositories.user_repo import get_all_users
+from app.database.repositories.user_repo import (
+    get_all_users,
+    get_user_by_telegram_id,
+    update_user_credits,
+)
+from app.config.constants import VALUE_DESCRIPTION, VALUE_AUDIO_TRANSCRIPTION
 from app.bot.handlers.daily import handle_morning
+from app.bot.lang.language import get_text
+
+ADMIN_CREDIT = 1
 
 
 async def handle_admin(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -20,6 +28,10 @@ async def handle_admin(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
                 InlineKeyboardButton(
                     f"Morning",
                     callback_data=f"admin_morning",
+                ),
+                InlineKeyboardButton(
+                    f"Credit User",
+                    callback_data=f"admin_credit_user",
                 ),
             ]
         ]
@@ -48,3 +60,48 @@ async def handle_admin_callback(
         elif query.data == "admin_morning":
             await handle_morning(context)
             await query.edit_message_text("Morning messages sent to all users.")
+        elif query.data == "admin_credit_user":
+            await query.edit_message_text(
+                "Please provide the Telegram ID and amount to credit in the format: <TelegramID> <Amount>"
+            )
+            return ADMIN_CREDIT
+
+    return ConversationHandler.END
+
+
+async def handle_admin_credit(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        user_id, amount = map(int, update.message.text.split())
+        target_user = get_user_by_telegram_id(user_id)
+
+        if not target_user:
+            await update.message.reply_text("Usuário não encontrado.")
+            return ConversationHandler.END
+
+        target_user = update_user_credits(
+            target_user, target_user.credit_balance + amount
+        )
+
+        await update.message.reply_text(
+            f"Créditos adicionados!\n{target_user.name}: {target_user.credit_balance}"
+        )
+
+        await context.bot.send_message(
+            chat_id=target_user.telegram_id,
+            text=get_text("pt_BR", "messages.credits.gift").format(
+                amount=amount, user_balance=target_user.credit_balance
+            ),
+        )
+        await context.bot.send_message(
+            chat_id=target_user.telegram_id,
+            text=get_text("pt_BR", "messages.menu").format(
+                user_balance=target_user.credit_balance if target_user else 0,
+                value_description=VALUE_DESCRIPTION,
+                value_audio=VALUE_AUDIO_TRANSCRIPTION,
+            ),
+        )
+
+    except Exception as e:
+        await update.message.reply_text(f"Erro: {e}")
+
+    return ConversationHandler.END
